38fe2aac158ea13b6f8670dd608e0ec4bf6f8cdf
UInt2048 <MBenedict2004@gmail.com>
2021-02-20 11:42:10 -0500

    Do stuff

--- macos-guest-virtualbox.sh
+++ macos-guest-virtualbox.sh
@@ -19,9 +19,9 @@ vm_name="macOS"                  # name of the VirtualBox virtual machine
 macOS_release_name="High Sierra" # install "HighSierra" "Mojave" or "Catalina"
 storage_size=64000               # VM disk image size in MB, minimum 22000
 storage_format="vdi"             # VM disk image file format, "vdi" or "vmdk"
-cpu_count=4                     # VM CPU cores, minimum 2
+cpu_count=4                      # VM CPU cores, minimum 2
 memory_size=8192                 # VM RAM in MB, minimum 2048
-gpu_vram=128                     # VM video RAM in MB, minimum 34, maximum 128
+gpu_vram=256                     # VM video RAM in MB, minimum 34, maximum 128
 resolution="1920x1200"           # VM display resolution
 
 # The following commented commands, when executed on a genuine Mac,
@@ -33,21 +33,21 @@ resolution="1920x1200"           # VM display resolution
 
 #   system_profiler SPHardwareDataType
 DmiSystemFamily="MacBook Pro"        # Model Name
-DmiSystemProduct="MacBookPro11,2"    # Model Identifier
-DmiSystemSerial="NO_DEVICE_SN"       # Serial Number (system)
-DmiSystemUuid="CAFECAFE-CAFE-CAFE-CAFE-DECAFFDECAFF" # Hardware UUID
-DmiBIOSVersion="string:MBP7.89"      # Boot ROM Version
+DmiSystemProduct="MacBookPro16,2"    # Model Identifier
+DmiSystemSerial="C02D1BEJML7H"       # Serial Number (system)
+DmiSystemUuid="9B1FDA64-541C-5BBB-9BF4-21EAE72E651A" # Hardware UUID
+DmiBIOSVersion="1554.40.33.0.0"      # Boot ROM Version
 DmiOEMVBoxVer="string:1"             # Apple ROM Info - left of the first dot
 DmiOEMVBoxRev="string:.23.45.6"      # Apple ROM Info - first dot and onward
 #   ioreg -l | grep -m 1 board-id
-DmiBoardProduct="Mac-3CBD00234E554E41"
+DmiBoardProduct="Mac-5F9802EFE386AA28"
 #   nvram 4D1EDE05-38C7-4A6A-9CC6-4BCCA8B38C14:MLB
-DmiBoardSerial="NO_LOGIC_BOARD_SN"    # stored in EFI
+DmiBoardSerial="C02026608BBP9KP1K"    # stored in EFI
 MLB="${DmiBoardSerial}"               # stored in NVRAM
 #   nvram 4D1EDE05-38C7-4A6A-9CC6-4BCCA8B38C14:ROM
-ROM='%aa*%bbg%cc%dd'
+ROM='x%cdw^%01%a7'
 #   ioreg -l -p IODeviceTree | grep \"system-id
-SYSTEM_UUID="aabbccddeeff00112233445566778899"
+SYSTEM_UUID="9c434ede07ace3558ada3b590a746b16"
 #   csrutil status
 SYSTEM_INTEGRITY_PROTECTION='10'  # '10' - enabled, '77' - disabled
 
